3b7c34ae63816a9f313a269ff245bfbbe7cf14be
Xabin Rodriguez <xabin.rodriguez@akting.eu>
2019-04-17 15:49:22 +0200

    refactoring

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,25 +1,30 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
-import { HEROES } from '../mock-heroes';
+import { HeroService } from '../hero.service';
 
 @Component({
-  selector: 'app-heroes',
-  templateUrl: './heroes.component.html',
-  styleUrls: ['./heroes.component.css']
+    selector: 'app-heroes',
+    templateUrl: './heroes.component.html',
+    styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent implements OnInit {
 
-    hero: Hero = {
-        id: 1,
-        name: 'Jorge el Botes',
-        ability: 'Fast falling'
-    };
+    heroes : Hero[];
+    selectedHero: Hero;
 
-    heroes = HEROES;
+    constructor(private heroService: HeroService) {
+        this.getHeroes();
+    }
 
-  constructor() { }
+    ngOnInit() {
+    }
 
-  ngOnInit() {
-  }
+    onSelect(hero: Hero): void {
+        this.selectedHero = hero;
+    }
+
+    getHeroes(): void {
+        this.heroes = this.heroService.getHeroes();
+    }
 
 }
