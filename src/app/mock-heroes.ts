296c4d171e66cc52b7981a99a38591fc393466a4
Xabin Rodriguez <xabin.rodriguez@akting.eu>
2019-04-23 08:55:57 +0200

    Dashboard component, routing

--- src/app/mock-heroes.ts
+++ src/app/mock-heroes.ts
@@ -11,6 +11,6 @@ export const HEROES: Hero[] = [
     {id: 8, name: 'Poltergeist', ability: 'He can join any VoIP application and use his cacophonies to distort reality and scare users.'},
     {id: 9, name: 'Tarthamud', ability: 'He is able to speak at a rate of 20 words per second, and can only be understood by fellow Tarthamuds. Tarthamuds were used as communication devices during WW2 to send encrypted messages'},
     {id: 10, name: 'Xan Who', ability: 'No special abilities, but his name is too funny to ignore'},
-    {id: 11, name: 'Xantiyo Abaxkal', ability: 'His signature ability Time Distortion allows him to exist outside of our current timeline. Precisely, his body exists in the current time, but his spirit habits the Atapuerk'},
+    {id: 11, name: 'Peru Abarka', ability: 'His signature ability Time Distortion allows him to exist outside of our current timeline. Precisely, his body exists in the current time, but his spirit habits the Atapuerk'},
     {id: 12, name: 'Notre Damn', ability: 'It burns faster than any other building. Can resist one lethal blow from Euskal Talde Su Ta Gar, but not two'},
 ]
